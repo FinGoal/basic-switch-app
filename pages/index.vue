a92b56bfd6aba1facbf1e23bc990ed8a6f48a969
jackdixonryan <jack.dixon.ryan@gmail.com>
2022-03-31 20:18:30 -0600

    Additional code comments in Index.vue.

--- pages/index.vue
+++ pages/index.vue
@@ -15,11 +15,11 @@ export default {
     }
   },
   async mounted() {
-    console.log(this.$config);
     const token = await this.generateToken();
     this.handler = this.createPlaidInstance(token);
   },
   methods: {
+    // Checks if there is a valid username, and generates a Link token if so.
     async generateToken() {
       try {
         const user = this.$store.state.user;
@@ -36,8 +36,11 @@ export default {
         console.log(error);
       }
     },
+    // Creates an instance of Plaid Link, using a valid link token.
     createPlaidInstance(link_token) {
-      const vm = this;
+      
+      const vm = this; // need to bind this to a variable, otherwise it rebinds to Plaid.create
+
       const handler = Plaid.create({
         token: link_token,
         onLoad() {
